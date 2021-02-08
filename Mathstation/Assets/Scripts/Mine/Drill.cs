0972a225a2ded1815e4ae987ab7fde8b665d0c36
Jonathan <jjjooonnn@gmail.com>
2021-02-08 10:45:55 +0100

    added the first scene for mine game. prototype-y, ugly, test if the elements from the design process make sense, and if they could yield a fun game activity

---
+++ Mathstation/Assets/Scripts/Mine/Drill.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drill : MonoBehaviour
+{
+    [SerializeField]
+    private float boostValue;
+
+    [SerializeField]
+    private GameObject resourceObject;
+
+    void OnMouseDown(){
+        resourceObject.GetComponent<Resource>().Boost(boostValue);
+    }
+}
---
+++ Mathstation/Assets/Scripts/Mine/Resource.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Resource : MonoBehaviour
+{
+    [SerializeField]
+    private float metals; //this is the game's resource for construction
+    [SerializeField]
+    private float passiveGainRate;
+    [SerializeField]
+    private GameObject textObject;
+
+
+    void Update(){
+        metals = Mathf.Clamp(metals, 0, 100);
+        textObject.GetComponent<Text>().text = ((int)metals).ToString();
+    }
+
+    void FixedUpdate(){
+        metals += passiveGainRate * Time.deltaTime;
+    }
+    
+
+    public void Consume(){}
+
+    public void Boost(float value){
+        metals += value;
+    }
+
+    public void Reduce(){}
+}
