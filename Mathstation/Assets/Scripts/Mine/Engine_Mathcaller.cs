61a0160350ec42c2569a7c90e9d02f77f0d3c7bc
Jonathan <jjjooonnn@gmail.com>
2021-02-20 18:35:42 +0100

    added initial mathcaller script for mine game - engine

---
+++ Mathstation/Assets/Scripts/Mine/Engine_Mathcaller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Engine_Mathcaller : MonoBehaviour, MathCaller
+{
+
+    [SerializeField]
+    private GameObject mathEditor;
+    [SerializeField]
+    private GameObject successObject;
+
+    public void CallMathEditor(){
+        GameObject editor = GameObject.Instantiate(mathEditor);
+        editor.GetComponent<EditorMaster>().SetReport(gameObject);
+        Time.timeScale=0;
+        GameObject.Find("MineGame").GetComponent<ClickLock>().Lock();
+    }
+
+    public void MathSuccess()
+    {
+        Time.timeScale=1;
+        GameObject.Find("MineGame").GetComponent<ClickLock>().Unlock();
+        successObject.SetActive(true);
+        GameObject.Find("MineGame").GetComponent<Engine>().DeployEngine();
+        gameObject.SetActive(false);
+    }
+}
