a725f68bb70cc478cfcd46e9c3b80e7172beaba5
Jonathan <jjjooonnn@gmail.com>
2021-03-08 16:02:04 +0100

    added a new scene for explore game, has player and some basic control scripts

---
+++ Mathstation/Assets/Explore/Scripts/ExplorePlayerControls.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplorePlayerControls : MonoBehaviour
+{
+    [SerializeField]
+    private float maxVelocity;
+
+    [SerializeField]
+    private GameObject leadingObject;
+    [SerializeField]
+    private float stopDistance;
+
+
+    void Update(){
+        if(leadingObject.activeSelf && Vector3.Distance(transform.position, leadingObject.transform.position) > stopDistance){
+            float lookAngle = Vector2.Angle(transform.up, (leadingObject.transform.position - transform.position).normalized);
+            transform.Rotate(new Vector3(0,0,lookAngle));
+            transform.Translate(Vector3.up * maxVelocity * Time.deltaTime ,Space.Self);
+        }
+    }
+
+}
