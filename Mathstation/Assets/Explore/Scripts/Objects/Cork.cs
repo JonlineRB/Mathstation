f88529c4b36e70f2b8a103801ef934e045be2fe2
Jonathan <jjjooonnn@gmail.com>
2021-05-04 19:50:57 +0200

    added a new script for a cork object, it is a subclass of carriable and it should be used to plug up the vortex. forgot to add the files in the last commit

---
+++ Mathstation/Assets/Explore/Scripts/Objects/Cork.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cork : Carriable
+{
+    // Start is called before the first frame update
+    protected override Carry.Carriables CarryObject()
+    {
+        return Carry.Carriables.None;
+    }
+}
