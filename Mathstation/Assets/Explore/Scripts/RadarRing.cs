725103e0310262a03c9fb955f8049bdd1d451bca
Jonathan <jjjooonnn@gmail.com>
2021-05-21 11:37:06 +0200

    added a radar ring object

---
+++ Mathstation/Assets/Explore/Scripts/RadarRing.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadarRing : MonoBehaviour
+{
+    [SerializeField] private GameObject[] fadies;
+    void OnTriggerEnter2D(Collider2D other){
+        foreach(GameObject fadie in fadies){
+            fadie.GetComponent<ConditionFade>().InitFadeIn();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other){
+        foreach(GameObject fadie in fadies){
+            fadie.GetComponent<ConditionFade>().InitFadeOut();
+        }
+    }
+}
---
+++ Mathstation/Assets/Scripts/General/ConditionFade.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConditionFade : MonoBehaviour
+{
+    //Script makes invisible (alpha=0) sprites visible based on condition
+    [SerializeField] private float fadeDuration;
+    [SerializeField] private Color defaultColor;
+    [SerializeField] private Color altColor;
+
+    void Start(){
+        gameObject.GetComponent<SpriteRenderer>().color = defaultColor;
+    }
+
+    public void InitFadeIn(){
+        StartCoroutine("FadeIn");
+    }
+
+    private IEnumerator FadeIn(){
+        float elapsed = 0f;
+
+        while(elapsed <= fadeDuration){
+            //interpolate forward
+            gameObject.GetComponent<SpriteRenderer>().color = Vector4.Lerp(defaultColor, altColor, elapsed / fadeDuration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    public void InitFadeOut(){
+        StartCoroutine("FadeOut");
+    }
+
+    private IEnumerator FadeOut(){
+        float elapsed = 0f;
+
+        while(elapsed <= fadeDuration){
+            //interpolate back
+            gameObject.GetComponent<SpriteRenderer>().color = Vector4.Lerp(altColor, defaultColor, elapsed / fadeDuration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
