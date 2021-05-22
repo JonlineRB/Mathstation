c2f7557d1729760f98a1f503d6993d60a935507b
Jonathan <jjjooonnn@gmail.com>
2021-05-22 20:19:59 +0200

    added arrows for the radar ring

--- Mathstation/Assets/Explore/Scripts/RadarRing.cs
+++ Mathstation/Assets/Explore/Scripts/RadarRing.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 public class RadarRing : MonoBehaviour
 {
-    [SerializeField] private GameObject[] fadies;
+    [SerializeField] private GameObject arrow;
+    [SerializeField] private float arrowDistance;
+    [SerializeField] private List<GameObject> fadies;
     // [SerializeField] private bool active = false;
+    [SerializeField] private GameObject[] pointingAt;
+
+    void Start(){
+        foreach(GameObject pointee in pointingAt){
+            //instantiate an arrow
+            //add it to the list of fade objects
+            Vector3 direction = (pointee.transform.position - transform.position).normalized;
+            GameObject arrowInstance = GameObject.Instantiate(arrow, transform.position + direction * arrowDistance,
+            Quaternion.Euler(0,0,Vector3.SignedAngle(Vector3.up, direction, Vector3.forward)), transform);
+            fadies.Add(arrowInstance);
+        }
+    }
     void OnTriggerEnter2D(Collider2D other){
         // if(!active){
         //     other.GetComponent<ArtifactInterpolation>().InitInterpolation(transform.position);
@@ -19,8 +33,6 @@ public class RadarRing : MonoBehaviour
         foreach(GameObject fadie in fadies){
             fadie.GetComponent<ConditionFade>().InitFadeIn();
         }
-        
-        
     }
     
     void OnTriggerExit2D(Collider2D other){
