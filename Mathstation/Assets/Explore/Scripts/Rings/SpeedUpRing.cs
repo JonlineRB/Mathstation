acae3afde4d615ef8b56204f10bb59d414f92036
Jonathan <jjjooonnn@gmail.com>
2021-05-23 15:48:25 +0200

    added text notifications when upgrading speed and when refueling in the explore game

--- Mathstation/Assets/Explore/Scripts/Rings/SpeedUpRing.cs
+++ Mathstation/Assets/Explore/Scripts/Rings/SpeedUpRing.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class SpeedUpRing : Ring
 {
+    [SerializeField] private GameObject SpeedUpNotification;
     protected override void ApplyRingEffect(){
         player.GetComponent<ExplorePlayerControls>().SpeedUp();
+        //instantiate the speed up notification
+        GameObject.Instantiate(SpeedUpNotification,transform.position, Quaternion.identity);
     }    
 }
