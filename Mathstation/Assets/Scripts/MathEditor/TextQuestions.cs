8d066c49be8140a2c7091b2887785ff1af0cb439
Jonathan <jjjooonnn@gmail.com>
2021-06-15 16:59:39 +0200

    added xml related test scene, scripts and the like

---
+++ Mathstation/Assets/Scripts/MathEditor/TextQuestions.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextQuestions : MonoBehaviour
+{
+    [SerializeField] string[] addition_questions;
+    [SerializeField] string[] subtraction_questions;
+    [SerializeField] string[] multiplication_questions;
+    [SerializeField] string[] division_questions;
+
+    public string ToTextProblem(Problem problem){
+        //break the problem string into tokens
+        string problemString = problem.ToString(false); //has the shape of 1 Add 1
+        if(problemString.Split(' ').Length!=3){
+            Debug.Log("Length is not 3");
+            return null;
+        }
+
+        string result = "";
+
+        switch(problemString.Split(' ')[1]){
+        case "Add":
+            result = addition_questions[0];
+            break;
+        case "Sub":
+            result = subtraction_questions[0];
+            break;
+        case "Mul":
+            result = multiplication_questions[0];
+            break;
+        case "Div":
+            result = division_questions[0];
+            break;
+        }
+
+        result = result.Replace("VAL_A", problemString.Split(' ')[0]);
+        result = result.Replace("VAL_B", problemString.Split(' ')[2]);
+
+        Debug.Log(problem.ToString(false));
+        return result;
+    }
+
+
+}
---
+++ Mathstation/Assets/XmlHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+
+public class XmlHandler : MonoBehaviour
+{
+
+    XmlDocument Xdoc = null;
+    
+    void Start()
+    {
+        Xdoc = new XmlDocument(); //Instantiate
+        Debug.Log("The current directory is: " + Application.dataPath);
+        Xdoc.Load(Application.dataPath + "/data.xml"); //Load XML file
+
+        XmlElement root = Xdoc.DocumentElement; //Get follow node
+        Debug.Log("The root element is: "+ root.Name);
+
+        XmlNode dataNode = root.SelectSingleNode("data"); //Get the child nodes under root
+        Debug.Log("Node name" + dataNode.Name);
+
+        // for(int i=0; i<dataNode.ChildNodes.Count; i++)
+        // {
+        //     Debug.Log("Text content " + dataNode.ChildNodes[i].InnerText); //Get text content
+        // }
+
+        int randomIndex = Random.Range(0, dataNode.ChildNodes.Count);
+
+        string result = dataNode.ChildNodes[randomIndex].InnerText;
+
+        Debug.Log("Random fetch content " + result);
+    }
+
+    
+}
