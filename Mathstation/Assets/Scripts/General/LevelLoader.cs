76398f53d0a7447b27afaf7fa09649443a59cf09
Jonathan <jjjooonnn@gmail.com>
2021-06-16 18:01:29 +0200

    added menu navigation to levels. need to complete level navigation back to menu.

---
+++ Mathstation/Assets/Scripts/General/LevelLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public static void LoadLevel(int index){
+        EditorSceneManager.LoadScene(index);
+    }
+
+    public static void LoadLevel(string name){
+        EditorSceneManager.LoadScene(name);
+    }
+}
