cc5d64f45e015fe1e4541e460e8870fd1a31eafa
Jonathan <jjjooonnn@gmail.com>
2021-06-17 21:19:19 +0200

    changed the button interaction to be scripted via new scripts to duplicate them more easily into other scenes of the same game

---
+++ Mathstation/Assets/Explore/Scripts/UI/CloseMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseMenu : MonoBehaviour
+{
+    public void UncallMenu(){
+        gameObject.GetComponent<TimeControl>().ResumeTime();
+        GameObject player = GameObject.Find("Player");
+        player.GetComponent<MoveLock>().DecrementMoveLock();
+        GameObject.Find("TargetMarker").SetActive(false);
+        player.GetComponent<Fuel>().SetConsuming(false);
+        gameObject.SetActive(false);
+    }
+}
---
+++ Mathstation/Assets/Explore/Scripts/UI/OpenMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenMenu : MonoBehaviour
+{
+    public void CallMenu(){
+        gameObject.SetActive(true);
+        gameObject.GetComponent<TimeControl>().StopTime();
+        GameObject.Find("Player").GetComponent<MoveLock>().IncrementMoveLock();
+    }
+}
