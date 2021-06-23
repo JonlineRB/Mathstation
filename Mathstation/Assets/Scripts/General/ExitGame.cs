5c86970cf0c96d325a58e12f8abe380113be405d
Jonathan <jjjooonnn@gmail.com>
2021-06-23 15:37:12 +0200

    added a help page to the main menu

---
+++ Mathstation/Assets/Scripts/General/ExitGame.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour
+{
+    //Quits the game
+    public void Execute(){
+        Application.Quit();
+    }
+}
