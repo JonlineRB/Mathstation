c6f3b57f3bb67a6b56811644261e14eec06167fa
Jonathan <jjjooonnn@gmail.com>
2021-07-09 18:01:35 +0200

    added cursor texture for fighting game, script that changes the cursor based on mouse enter / exit. implemented for quizshow

---
+++ Mathstation/Assets/Scripts/Fight/MouseOverCursorChange.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseOverCursorChange : MonoBehaviour
+{
+    [SerializeField] private Texture2D cursorTexture;
+
+    void OnMouseEnter(){
+        Cursor.SetCursor(cursorTexture, new Vector2(0.5f, 0.5f) * cursorTexture.height, CursorMode.ForceSoftware);
+    }
+
+    void OnMouseExit(){
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+    
+}
