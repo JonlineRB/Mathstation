6032472e9d64f724fcbb5b4a06895636839fb2e5
Jonathan <jjjooonnn@gmail.com>
2021-07-22 17:01:06 +0200

    modified math editor master script to accomodate stand alone math editor. In this mode, the math editor counts problems up instead of down. There is now a quit button so that the player can leave the scene. Also improved the visibility of the math editor

--- Mathstation/Assets/Scripts/MathEditor/EditorMaster.cs
+++ Mathstation/Assets/Scripts/MathEditor/EditorMaster.cs
@@ -10,24 +10,41 @@ public class EditorMaster : MonoBehaviour
     [SerializeField] public int problemCount;
     [SerializeField] private GameObject problemCountObject; // UI text value
     [SerializeField] private GameObject reportsTo; // Object to notify when math has been solved
+    [SerializeField] private bool isStandalone; // Is set to true in the math scene (no game, just math)
     void Start()
     {
         //generate initial problem
         GenerateProblem();
+
+        if(isStandalone){
+            problemCount = 0;
+        }
+
         problemCountObject.GetComponent<Text>().text = problemCount.ToString();
     }
 
     public void Submition(){
-        DecrementProblemCount();
+        if(isStandalone)
+            IncrementProblemCount();
+        else
+            DecrementProblemCount();
         Debug.Log("problemCount is: " + problemCount);
     }
 
+    // Used in game to reduce the problem counts to 0, and close the editor
     private void DecrementProblemCount(){
         if(--problemCount <= 0){
             reportsTo.GetComponent<MathCaller>().MathSuccess();
             GameObject.Destroy(gameObject);
             return;
         }
+        problemCountObject.GetComponent<Text>().text = problemCount.ToString();
+        GenerateProblem();
+    }
+
+    // Used in standalone mode to count how many problems have been solved so far
+    private void IncrementProblemCount(){
+        problemCount++;
         problemCountObject.GetComponent<Text>().text = problemCount.ToString();
         GenerateProblem();
     }
