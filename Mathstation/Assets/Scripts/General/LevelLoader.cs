b7a2e2383116c1b1620c7312d5638a4332cc276d
Jonathan <jjjooonnn@gmail.com>
2021-07-22 21:58:15 +0200

    cleaned up level loader to use scene management, as opposed to editor load scene

--- Mathstation/Assets/Scripts/General/LevelLoader.cs
+++ Mathstation/Assets/Scripts/General/LevelLoader.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
 
 // Script loads levels.
 // Used with buttons.
@@ -9,12 +9,12 @@ public class LevelLoader : MonoBehaviour
 {
     // Load level by build index
     public static void LoadLevel(int index){
-        EditorSceneManager.LoadScene(index);
+        SceneManager.LoadScene(index);
     }
 
     // Load level by name.
     // Example: "Scenes/Menu".
     public static void LoadLevel(string name){
-        EditorSceneManager.LoadScene(name);
+        SceneManager.LoadScene(name);
     }
 }
